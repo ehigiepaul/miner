9d07d03415c137c906d884bc799154042d077a77
Paul Ehigie Paul <ehigiepaul@bracketcore.com>
2017-10-29 14:18:39 +0100

    release clean

--- dev-src/debbuger.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from "fs";
-import * as path from "path";
-var appLocation = path.resolve(__dirname, "../../");
-
-let watcher = fs.watch(appLocation, () => {
-  nw.Window.get().showDevTools();
-  watcher.close();
-});
-console.log("tessting");
